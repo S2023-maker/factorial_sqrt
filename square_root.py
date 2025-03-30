890cdc58ab08ea5498a04cd10b000cb4b31581bd
S2023-maker <shreyarjoshi1995@gmail.com>
2025-03-30 20:10:59 +0530

    Add files via upload

---
+++ square_root.py
@@ -0,0 +1,13 @@
+import math
+
+number = eval(input("Enter a number: "))
+
+sqrt_value = math.sqrt(number)
+
+log_value = math.log(number)
+
+sine_value = math.sin(number)
+
+print("Square root :", sqrt_value)
+print("Logarithm: ",log_value)
+print("Sine :", sine_value)
